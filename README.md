a2fbcb8d90036c790b8e4e7447ce094f3174a808
Luke <32024335+lukeeey@users.noreply.github.com>
2021-01-31 15:50:56 +0000

    Add license, readme and support for teleporting to a specific world

---
+++ README.md
@@ -0,0 +1,7 @@
+# DeathView
+A Nukkit plugin that puts you into spectator made when you die, bypassing the death screen.
+
+## Permissions
+| Permission | Default |
+| ---------- | ------- |
+| deathview.spectator | true |
--- src/main/java/io/github/lukeeey/deathview/DeathView.java
+++ src/main/java/io/github/lukeeey/deathview/DeathView.java
@@ -1,4 +1,4 @@
-package lukeeey.nukkit.deathview;
+package io.github.lukeeey.deathview;
 
 import cn.nukkit.Player;
 import cn.nukkit.entity.Entity;
@@ -6,6 +6,8 @@ import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Location;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.plugin.PluginBase;
 
@@ -16,23 +18,23 @@ public class DeathView extends PluginBase implements Listener {
         saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
 
-        if(getConfig().getBoolean("teleport-to-spawn") && getConfig().getBoolean("teleport-to-coords")) {
+        if (getConfig().getBoolean("teleport-to-spawn") && getConfig().getBoolean("teleport-to-coords")) {
             throw new RuntimeException("Teleporting to spawn enabled in config, but teleporting to specific coordinates is also enabled.");
         }
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onEntityDamage(EntityDamageEvent event) {
-        if(event.isCancelled()) {
+        if (event.isCancelled()) {
             return;
         }
 
         Entity entity = event.getEntity();
 
-        if(entity instanceof Player && entity.getHealth() - event.getDamage() <= 0) {
+        if (entity instanceof Player && entity.getHealth() - event.getDamage() <= 0) {
             Player player = (Player) entity;
 
-            if(player.getGamemode() == 1) {
+            if (player.getGamemode() == 1 || !player.hasPermission("deathview.spectator")) {
                 return;
             }
 
@@ -42,11 +44,11 @@ public class DeathView extends PluginBase implements Listener {
             String playerMessage = getConfig().getString("death-message.died.player");
             String allMessage = getConfig().getString("death-message.died.all");
 
-            if(!playerMessage.isEmpty()) {
+            if (!playerMessage.isEmpty()) {
                 player.sendMessage(placeholder(event, playerMessage));
             }
 
-            if(!allMessage.isEmpty()) {
+            if (!allMessage.isEmpty()) {
                 getServer().broadcastMessage(placeholder(event, allMessage));
             }
 
@@ -54,12 +56,14 @@ public class DeathView extends PluginBase implements Listener {
                 player.setGamemode(0);
                 player.heal(20);
 
-                if(getConfig().getBoolean("teleport-to-spawn")) {
-                    player.teleport(getServer().getDefaultLevel().getSafeSpawn());
+                Level world = findWorld();
+
+                if (getConfig().getBoolean("teleport-to-spawn")) {
+                    player.teleport(world.getSafeSpawn());
                 }
-                if(getConfig().getBoolean("teleport-to-coords")) {
-                    player.teleport(new Vector3(getConfig().getInt("teleport-to-coords.x"),
-                            getConfig().getInt("teleport-to-coords.y"), getConfig().getInt("teleport-to-coords.z")));
+                if (getConfig().getBoolean("teleport-to-coords")) {
+                    player.teleport(new Location(getConfig().getInt("teleport-to-coords.x"),
+                            getConfig().getInt("teleport-to-coords.y"), getConfig().getInt("teleport-to-coords.z"), 0, 0, world));
                 }
             }, getConfig().getInt("time") * 20);
         }
@@ -69,5 +73,20 @@ public class DeathView extends PluginBase implements Listener {
         return text
                 .replace("{victim}", event.getEntity().getName())
                 .replace("{world}", event.getEntity().getLevel().getName());
+    }
+
+    private Level findWorld() {
+        Level defaultLevel = getServer().getDefaultLevel();
+        String configWorld = getConfig().getString("teleport-to-world", "default");
+
+        if (configWorld.equalsIgnoreCase("default")) {
+            return defaultLevel;
+        }
+
+        Level level = getServer().getLevelByName(configWorld);
+        if (level == null) {
+            throw new RuntimeException("Invalid world name specified in config: " + configWorld);
+        }
+        return level;
     }
 }
